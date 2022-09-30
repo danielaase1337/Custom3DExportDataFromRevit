6ed0161a2712bc6850751ab91683999e7b44c87c
Daniel Aase <daniel.aase@focus.no>
2022-09-30 12:19:59 +0200

    Updated to Revit2023, and added Focus Signing. Added Revit dependencies to solution

--- CustomExporterAdnMeshJson/Command.cs
+++ CustomExporterAdnMeshJson/Command.cs
@@ -8,99 +8,118 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System.IO;
+using System.Reflection;
 #endregion
 
 namespace CustomExporterAdnMeshJson
 {
-  /// <summary>
-  /// ADN mesh data custom exporter 
-  /// external command mainline.
-  /// </summary>
-  [Transaction( TransactionMode.ReadOnly )]
-  public class Command : IExternalCommand
-  {
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
+    /// <summary>
+    /// ADN mesh data custom exporter 
+    /// external command mainline.
+    /// </summary>
+    [Transaction(TransactionMode.ReadOnly)]
+    public class Command : IExternalCommand
     {
-      UIApplication uiapp = commandData.Application;
-      UIDocument uidoc = uiapp.ActiveUIDocument;
-      Application app = uiapp.Application;
-      Document doc = uidoc.Document;
-
-      // This command requires an active document
-
-      if( null == uidoc )
-      {
-        message = "Please run this command in an active project document.";
-        return Result.Failed;
-      }
-
-      View3D view = doc.ActiveView as View3D;
-
-      if( null == view )
-      {
-        message = "Please run this command in a 3D view.";
-        return Result.Failed;
-      }
-
-      // Instantiate our custom context
-
-      ExportContextAdnMesh context 
-        = new ExportContextAdnMesh( doc );
-
-      // Instantiate a custom exporter with it
-
-      using( CustomExporter exporter
-        = new CustomExporter( doc, context ) )
-      {
-        // Tell the exporter whether we need face info.
-        // If not, it is better to exclude them, since 
-        // processing faces takes significant time and 
-        // memory. In any case, tessellated polymeshes
-        // can be exported (and will be sent to the 
-        // context). Excluding faces just excludes the calls, 
-        // not the actual processing of face tessellation. 
-        // Meshes of the faces will still be received by 
-        // the context.
-
-        //exporter.IncludeFaces = false; // removed in Revit 2017
-
-        exporter.IncludeGeometricObjects = false; // Revit 2017
-
-        try
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
         {
-          exporter.Export( view );
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // This command requires an active document
+
+            if (null == uidoc)
+            {
+                message = "Please run this command in an active project document.";
+                return Result.Failed;
+            }
+
+            View3D view = doc.ActiveView as View3D;
+
+            if (null == view)
+            {
+                message = "Please run this command in a 3D view.";
+                return Result.Failed;
+            }
+
+            // Instantiate our custom context
+
+            ExportContextAdnMesh context
+              = new ExportContextAdnMesh(doc);
+
+            // Instantiate a custom exporter with it
+
+            using (CustomExporter exporter = new CustomExporter(doc, context))
+            {
+                // Tell the exporter whether we need face info.
+                // If not, it is better to exclude them, since 
+                // processing faces takes significant time and 
+                // memory. In any case, tessellated polymeshes
+                // can be exported (and will be sent to the 
+                // context). Excluding faces just excludes the calls, 
+                // not the actual processing of face tessellation. 
+                // Meshes of the faces will still be received by 
+                // the context.
+
+                //exporter.IncludeFaces = false; // removed in Revit 2017
+
+                exporter.IncludeGeometricObjects = false; // Revit 2017
+               
+                try
+                {
+                    exporter.Export(view);
+                }
+                catch (Autodesk.Revit.Exceptions.ExternalApplicationException ex)
+                {
+                    Debug.Print("ExternalApplicationException " + ex.Message);
+                }
+            }
+
+
+            string storePath = String.Empty;
+            var saveDlg = new System.Windows.Forms.SaveFileDialog(); 
+            saveDlg.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            saveDlg.RestoreDirectory = true; 
+            var res = saveDlg.ShowDialog();
+            if (res == System.Windows.Forms.DialogResult.OK)
+                storePath = saveDlg.FileName;
+
+            if (string.IsNullOrEmpty(storePath))
+            {
+                var executingPath = Assembly.GetExecutingAssembly().Location;
+                var pathAbove = Directory.GetParent(executingPath);
+                var outputDataFolder = Path.Combine(pathAbove.FullName, "OutputDir");
+                if (!Directory.Exists(outputDataFolder))
+                    Directory.CreateDirectory(outputDataFolder);
+
+                storePath = Path.Combine(outputDataFolder, "Exported3dTestData.json");
+            }
+
+            
+                StreamWriter s = new StreamWriter(
+              storePath);
+
+            s.Write("[");
+
+            int i = 0;
+
+            foreach (AdnMeshData d in context.MeshData)
+            {
+                if (0 < i) { s.Write(','); }
+
+                s.Write(d.ToJson());
+
+                ++i;
+            }
+
+            s.Write("\n]\n");
+            s.Close();
+
+            return Result.Succeeded;
         }
-        catch( Autodesk.Revit.Exceptions.ExternalApplicationException ex )
-        {
-          Debug.Print( "ExternalApplicationException " + ex.Message );
-        }
-      }
-
-      // Save ADN mesh data in JSON format
-
-      StreamWriter s = new StreamWriter( 
-        "C:/tmp/test.json" );
-
-      s.Write( "[" );
-
-      int i = 0;
-
-      foreach( AdnMeshData d in context.MeshData )
-      {
-        if( 0 < i ) { s.Write( ',' ); }
-
-        s.Write( d.ToJson() );
-
-        ++i;
-      }
-
-      s.Write( "\n]\n" );
-      s.Close();
-
-      return Result.Succeeded;
     }
-  }
 }
--- CustomExporterAdnMeshJson/PointInt.cs
+++ CustomExporterAdnMeshJson/PointInt.cs
@@ -3,54 +3,54 @@ using Autodesk.Revit.DB;
 
 namespace CustomExporterAdnMeshJson
 {
-  /// <summary>
-  /// An integer-based 3D point class.
-  /// </summary>
-  class PointInt : IComparable<PointInt>
-  {
-    public int X { get; set; }
-    public int Y { get; set; }
-    public int Z { get; set; }
-
-    //public PointInt( int x, int y, int z )
-    //{
-    //  X = x;
-    //  Y = y;
-    //  Z = z;
-    //}
-
-    const double _feet_to_mm = 25.4 * 12;
-
-    static int ConvertFeetToMillimetres( double d )
-    {
-      return (int) ( _feet_to_mm * d + 0.5 );
-    }
-
     /// <summary>
-    /// Create an integer-based point in millimetres
-    /// from a given point in imperial coordinates.
+    /// An integer-based 3D point class.
     /// </summary>
-    public PointInt( XYZ p )
+    class PointInt : IComparable<PointInt>
     {
-      X = ConvertFeetToMillimetres( p.X );
-      Y = ConvertFeetToMillimetres( p.Y );
-      Z = ConvertFeetToMillimetres( p.Z );
-    }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
 
-    public int CompareTo( PointInt a )
-    {
-      int d = X - a.X;
+        //public PointInt( int x, int y, int z )
+        //{
+        //  X = x;
+        //  Y = y;
+        //  Z = z;
+        //}
 
-      if( 0 == d )
-      {
-        d = Y - a.Y;
+        const double _feet_to_mm = 25.4 * 12;
 
-        if( 0 == d )
+        static int ConvertFeetToMillimetres(double d)
         {
-          d = Z - a.Z;
+            return (int)UnitUtils.ConvertFromInternalUnits(d, UnitTypeId.Millimeters);
         }
-      }
-      return d;
+
+        /// <summary>
+        /// Create an integer-based point in millimetres
+        /// from a given point in imperial coordinates.
+        /// </summary>
+        public PointInt(XYZ p)
+        {
+            X = ConvertFeetToMillimetres(p.X);
+            Y = ConvertFeetToMillimetres(p.Y);
+            Z = ConvertFeetToMillimetres(p.Z);
+        }
+
+        public int CompareTo(PointInt a)
+        {
+            int d = X - a.X;
+
+            if (0 == d)
+            {
+                d = Y - a.Y;
+
+                if (0 == d)
+                {
+                    d = Z - a.Z;
+                }
+            }
+            return d;
+        }
     }
-  }
 }
